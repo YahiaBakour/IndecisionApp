31d1efda0d743673c85ca9320bdcb7963cc2ab96
Yahia Bakour <39255989+YahiaBakour@users.noreply.github.com>
2020-05-11 14:57:32 -0400

    Updates

---
+++ precache-manifest.163c33a27de3f42a5e5cec53808c5c66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bde40c0e0cfcd30be36a40541d6493d",
+    "url": "/monsters-react-test/index.html"
+  },
+  {
+    "revision": "7ad0d1443083a828f987",
+    "url": "/monsters-react-test/static/css/main.5822345c.chunk.css"
+  },
+  {
+    "revision": "ca96cb1d2607882b95b5",
+    "url": "/monsters-react-test/static/js/2.9a317882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react-test/static/js/2.9a317882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ad0d1443083a828f987",
+    "url": "/monsters-react-test/static/js/main.dd142730.chunk.js"
+  },
+  {
+    "revision": "d519e8d82209f687c57b",
+    "url": "/monsters-react-test/static/js/runtime-main.14de360c.js"
+  }
+]);
